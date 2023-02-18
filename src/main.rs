c68da7cce56fb300e3d69748a103618daa464067
Grzegorz Czyrnek <grzegorz.czyrnek@gmail.com>
2023-02-18 18:52:28 +0100

    Shorten arduino_hal to ah alias

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,25 @@
 
 #![cfg_attr(not(test), no_main)]
 
+use ah::Peripherals;
 #[cfg(not(test))]
 use panic_halt as _;
 use ufmt;
+use arduino_hal as ah;
 
 //#[cfg(not(test))]
-#[arduino_hal::entry]
+#[ah::entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
+    let dp = ah::Peripherals::take().unwrap();
+    let pins = ah::pins!(dp);
 
-    let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
-    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
+    let mut serial = ah::default_serial!(dp, pins, 115200);
+    let mut adc = ah::Adc::new(dp.ADC, Default::default());
     let water_level = pins.a2.into_analog_input(&mut adc);
     let soil_moisture = pins.a1.into_analog_input(&mut adc);
-    
+
     loop {
-        arduino_hal::delay_ms(1000);
+        ah::delay_ms(1000);
         let voltage = water_level.analog_read(&mut adc);
         ufmt::uwriteln!(&mut serial, "Water level: {}", voltage).unwrap();
         let voltage = soil_moisture.analog_read(&mut adc);
