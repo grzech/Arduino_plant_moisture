8ba250eed32e37bceb0b781a9c0bff87e5c1c7c3
Grzegorz Czyrnek <grzegorz.czyrnek@gmail.com>
2023-02-18 18:40:47 +0100

    Add reading two channels independently

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 use panic_halt as _;
 use ufmt;
 
-#[cfg(not(test))]
+//#[cfg(not(test))]
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
@@ -15,10 +15,14 @@ fn main() -> ! {
     let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
     let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
     let water_level = pins.a2.into_analog_input(&mut adc);
+    let soil_moisture = pins.a1.into_analog_input(&mut adc);
+    
     loop {
         arduino_hal::delay_ms(1000);
         let voltage = water_level.analog_read(&mut adc);
-        ufmt::uwriteln!(&mut serial, "Voltage from ADC: {}", voltage).unwrap();
+        ufmt::uwriteln!(&mut serial, "Water level: {}", voltage).unwrap();
+        let voltage = soil_moisture.analog_read(&mut adc);
+        ufmt::uwriteln!(&mut serial, "Soil moisture: {}", voltage).unwrap();
     }
 }
 
