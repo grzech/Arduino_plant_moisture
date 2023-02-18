0a1d66df5cd94622a8e4d0f4e29113af0e25c8c7
Grzegorz Czyrnek <grzegorz.czyrnek@gmail.com>
2023-02-18 17:46:12 +0100

    Add Serial for debugging purposes

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 #[cfg(not(test))]
 use panic_halt as _;
+use ufmt;
 
 #[cfg(not(test))]
 #[arduino_hal::entry]
@@ -12,10 +13,17 @@ fn main() -> ! {
     let pins = arduino_hal::pins!(dp);
 
     let mut led = pins.d13.into_output();
-
+    let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
+    let mut i = 0u16;
     loop {
         led.toggle();
         arduino_hal::delay_ms(1000);
+        ufmt::uwriteln!(&mut serial, "Hello for {} time", i).unwrap();
+        if i < u16::MAX {
+            i += 1;
+        } else {
+            i = 0;
+        }
     }
 }
 
